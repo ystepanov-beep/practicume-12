de4d2ae0568e4d49440fd8f20716fd815d6d4b80
ystepanov-beep <y.stepanov@g.nsu.ru>
2026-04-25 12:36:30 +0700

    Update zadanie 2.py

--- zadanie 2.py
+++ zadanie 2.py
@@ -1,4 +1,7 @@
-def count(n):
+def count(n: int) -> int:
+    """
+    Рекурсивная функция, которая считает количество цифр в натуральном числе n.
+    """
     if len(str(n)) == 1:
         return 1
     return 1 + count(n // 10) 
